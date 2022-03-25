5643048ddc09121077e289738c7076871d12901f
Vincent Muriuki <vincentmuriuki@Vincents-MacBook-Air.local>
2022-03-26 01:47:47 +0300

    Endpoint schema

---
+++ src/utils/schema.ts
@@ -0,0 +1,21 @@
+import * as Joi from "@hapi/joi";
+
+export const schemaErrorMessage = (message: any) => () => message;
+
+const signupSchema = Joi.object()
+  .keys({
+    firstName: Joi.string().strict().trim(),
+    lasstName: Joi.string().strict().trim(),
+    email: Joi.string().strict().trim().email().required(),
+    password: Joi.string()
+      .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/)
+      .min(8)
+      .required(),
+  })
+  .options({
+    abortEarly: false,
+  });
+
+const schema: Record<string, any> = {
+  "/auth/signup": signupSchema,
+};
